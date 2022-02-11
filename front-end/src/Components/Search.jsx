2051584964a84c9f35820c64de90ac14587729e4
Nancy Boktor <nancyboktor1@gmail.com>
2022-02-11 00:46:17 -0500

    Adding some style to the search component

--- front-end/src/Components/Search.jsx
+++ front-end/src/Components/Search.jsx
@@ -5,13 +5,24 @@ import InputBase from "@mui/material/InputBase";
 import IconButton from "@mui/material/IconButton";
 
 import SearchIcon from "@mui/icons-material/Search";
-
+import { indigo } from "@mui/material/colors";
 
 export default function Search({ onChange, value, onClick }) {
   return (
     <Paper
       component="form"
-      sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 400 }}
+      sx={{
+        p: "2px 4px",
+        display: "flex",
+        alignItems: "center",
+        width: 400,
+        backgroundColor: indigo["300"],
+        flexDirection: "row",
+        justifyContent: "center",
+        margin: "auto",
+        marginTop: "50px",
+        marginBottom: "60px",
+      }}
     >
       <InputBase
         sx={{ ml: 1, flex: 1 }}
